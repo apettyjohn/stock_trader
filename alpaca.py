52a15554d6bf7affc2a2572e35ab23cfdfb44340
Adam Pettyjohn <apettyjohn7@gmail.com>
2021-01-29 11:05:23 -0500

    Added many more Alpaca functions, yet to be tested
    Added watchlist,calendar,account configuration, and
    portfolio request functions

--- alpaca.py
+++ alpaca.py
@@ -27,29 +27,27 @@ ORDERS_URL = '{}/v2/orders'.format(BASE_URL)
 CLOCK_URL = '{}/v2/clock'.format(BASE_URL)
 POSITIONS_URL = '{}/v2/positions'.format(BASE_URL)
 ASSETS_URL = '{}/v2/assets'.format(BASE_URL)
+CALENDAR_URL = '{}/v2/calendar'.format(BASE_URL)
+WATCHLIST_URL = '{}/v2/watchlists'.format(BASE_URL)
 
 # Account Functions
 def checkAccount():
     # return account info
     r = requests.get(ACCOUNT_URL,headers=HEADERS)
     return json.loads(r.content)
-
-# Clock Functions
-def marketOpen():
-    r = requests.get(CLOCK_URL,headers=HEADERS)
-    return json.loads(r.content)['is_open']
-def getClock():
-    r = requests.get(CLOCK_URL,headers=HEADERS)
+def getAccountConfigs():
+    r = requests.get(ACCOUNT_URL + '/configurations',headers=HEADERS)
     return json.loads(r.content)
-
-# Asset Functions
-def getAsset(id):
-    if type(id) == str:
-        r = requests.get(ASSETS_URL + '/{{}}'.format(id),headers=HEADERS)
-    elif type(id) == int:
-        r = requests.get(ASSETS_URL + '/:{}'.format(id),headers=HEADERS)
-    else:
-        r = "Invalid input for getAsset()"
+def updateAccountConfigs(data):
+    r = requests.patch(ACCOUNT_URL + '/configurations',json=data,headers=HEADERS)
+    return json.loads(r.content)
+def portfolioHistory(parameters):
+    # Parameters include:
+    # period - The duration of the data in <number>+<unit>, D for day, W for week, M for month and A for year
+    # timeframe - The resolution of time window. 1Min, 5Min, 15Min, 1H, or 1D
+    # date_end - in “YYYY-MM-DD” format. Default is current day
+    # extended_hours - bool, timeframe must be < 1D
+    r = requests.get(ACCOUNT_URL + '/portfolio/history',headers=HEADERS)
     return json.loads(r.content)
 
 # Order Functions
@@ -57,7 +55,7 @@ def getOrder(id):
     if id == 'all':
         r = requests.get(ORDERS_URL,headers=HEADERS)
     else:
-        r = requests.get(ORDERS_URL + '/{}'.format({id}),headers=HEADERS)
+        r = requests.get(ORDERS_URL + '/{}'.format(id),headers=HEADERS)
     return json.loads(r.content)
 def postOrder(symbol,qty,side,type,time_in_force,other_field='',other_field_value=0,extended_hrs=False):
     data = {
@@ -75,7 +73,7 @@ def postOrder(symbol,qty,side,type,time_in_force,other_field='',other_field_valu
     r = requests.post(ORDERS_URL,json=data,headers=HEADERS)
     return json.loads(r.content)
 def patchOrder(id,update_fields):
-    r = requests.patch(ORDERS_URL + '/{}'.format({id}),json=update_fields,headers=HEADERS)
+    r = requests.patch(ORDERS_URL + '/{}'.format(id),json=update_fields,headers=HEADERS)
     return json.loads(r.content)
 def deleteOrder(id):
     if id == 'all':
@@ -89,13 +87,63 @@ def getPosition(id):
     if id == 'all':
         r = requests.get(POSITIONS_URL,headers=HEADERS)
     else:
-        r = requests.get(POSITIONS_URL + '/{}'.format({id}),headers=HEADERS)
+        r = requests.get(POSITIONS_URL + '/{}'.format(id),headers=HEADERS)
     return json.loads(r.content)
 def deletePosition(id):
     if id == 'all':
         r = requests.delete(POSITIONS_URL,headers=HEADERS)
     else:
         r = requests.delete(POSITIONS_URL + f'/{id}',headers=HEADERS)
+    return json.loads(r.content)
+
+# Asset Functions
+def getAsset(id):
+    if type(id) == str:
+        r = requests.get(ASSETS_URL + '/{}'.format(id),headers=HEADERS)
+    elif type(id) == int:
+        r = requests.get(ASSETS_URL + '/:{}'.format(id),headers=HEADERS)
+    else:
+        r = "Invalid input for getAsset()"
+    return json.loads(r.content)
+
+# Watchlist Functions
+def getWatchlist(id):
+    if id == 'all':
+        r = requests.get(WATCHLIST_URL,headers=HEADERS)
+    else:
+        r = requests.get(WATCHLIST_URL + '/{}'.format(id),headers=HEADERS)
+    return json.loads(r.content)
+def newWatchlist(name,symbols=[]):
+    r = requests.post(WATCHLIST_URL,json={'name':name,'symbols':symbols},headers=HEADERS)
+    return json.loads(r.content)
+def updateWatchlist(id,*name,symbols=[]):
+    r = requests.get(WATCHLIST_URL + '/{}'.format(id),headers=HEADERS)
+    if type(name) == None:
+        name = r['name']
+    r = requests.put(WATCHLIST_URL + '/{}'.format(id),json={'name':name,'symbols':symbols},headers=HEADERS)
+    return json.loads(r.content)
+def addWatchlistSymbol(id,symbol):
+    r = requests.post(WATCHLIST_URL + '/{}'.format(id),json={'symbol':symbol},headers=HEADERS)
+    return json.loads(r.content)
+def removeWatchlistSymbol(id,symbol):
+    r = requests.delete(WATCHLIST_URL + f'/{id}/{symbol}',headers=HEADERS)
+    return json.loads(r.content)
+def deleteWatchlist(id):
+    r = requests.delete(WATCHLIST_URL + f'/{id}',headers=HEADERS)
+    return json.loads(r.content)
+
+# Calendar Functions
+def getCalendar(start_date,end_date):
+    # dates must be in "%Y-%m-%d" format
+    r = requests.get(CALENDAR_URL,json={'start':start_date,'end':end_date},headers=HEADERS)
+    return json.loads(r.content)
+
+# Clock Functions
+def marketOpen():
+    r = requests.get(CLOCK_URL,headers=HEADERS)
+    return json.loads(r.content)['is_open']
+def getClock():
+    r = requests.get(CLOCK_URL,headers=HEADERS)
     return json.loads(r.content)
 
 # Other Functions
